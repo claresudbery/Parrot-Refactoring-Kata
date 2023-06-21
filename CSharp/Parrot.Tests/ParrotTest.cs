596380964b9f0a7e7ac3631f6ee545606e70b6fb
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-06-21 21:46:40 +0100

    Remove redundant cast expressions

--- CSharp/Parrot.Tests/ParrotTest.cs
+++ CSharp/Parrot.Tests/ParrotTest.cs
@@ -7,49 +7,49 @@ namespace Parrot.Tests
         [Fact]
         public void GetSpeedNorwegianBlueParrot_nailed()
         {
-            var parrot = Parrot.CreateInstance((ParrotTypeEnum)ParrotTypeEnum.NorwegianBlue, (int)0, (double)0, (bool)true);
+            var parrot = Parrot.CreateInstance(ParrotTypeEnum.NorwegianBlue, 0, 0, true);
             Assert.Equal(0.0, parrot.GetSpeed());
         }
 
         [Fact]
         public void GetSpeedNorwegianBlueParrot_not_nailed()
         {
-            var parrot = Parrot.CreateInstance((ParrotTypeEnum)ParrotTypeEnum.NorwegianBlue, (int)0, (double)1.5, (bool)false);
+            var parrot = Parrot.CreateInstance(ParrotTypeEnum.NorwegianBlue, 0, 1.5, false);
             Assert.Equal(18.0, parrot.GetSpeed());
         }
 
         [Fact]
         public void GetSpeedNorwegianBlueParrot_not_nailed_high_voltage()
         {
-            var parrot = Parrot.CreateInstance((ParrotTypeEnum)ParrotTypeEnum.NorwegianBlue, (int)0, (double)4, (bool)false);
+            var parrot = Parrot.CreateInstance(ParrotTypeEnum.NorwegianBlue, 0, 4, false);
             Assert.Equal(24.0, parrot.GetSpeed());
         }
 
         [Fact]
         public void GetSpeedOfAfricanParrot_With_No_Coconuts()
         {
-            var parrot = Parrot.CreateInstance((ParrotTypeEnum)ParrotTypeEnum.African, (int)0, (double)0, (bool)false);
+            var parrot = Parrot.CreateInstance(ParrotTypeEnum.African, 0, 0, false);
             Assert.Equal(12.0, parrot.GetSpeed());
         }
 
         [Fact]
         public void GetSpeedOfAfricanParrot_With_One_Coconut()
         {
-            var parrot = Parrot.CreateInstance((ParrotTypeEnum)ParrotTypeEnum.African, (int)1, (double)0, (bool)false);
+            var parrot = Parrot.CreateInstance(ParrotTypeEnum.African, 1, 0, false);
             Assert.Equal(3.0, parrot.GetSpeed());
         }
 
         [Fact]
         public void GetSpeedOfAfricanParrot_With_Two_Coconuts()
         {
-            var parrot = Parrot.CreateInstance((ParrotTypeEnum)ParrotTypeEnum.African, (int)2, (double)0, (bool)false);
+            var parrot = Parrot.CreateInstance(ParrotTypeEnum.African, 2, 0, false);
             Assert.Equal(0.0, parrot.GetSpeed());
         }
 
         [Fact]
         public void GetSpeedOfEuropeanParrot()
         {
-            var parrot = Parrot.CreateInstance((ParrotTypeEnum)ParrotTypeEnum.European, (int)0, (double)0, (bool)false);
+            var parrot = Parrot.CreateInstance(ParrotTypeEnum.European, 0, 0, false);
             Assert.Equal(12.0, parrot.GetSpeed());
         }
     }

90119e27ca89ef89a248b8bafb52c3fb38058a14
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-06-22 14:00:47 +0100

    Remove unused member var

--- CSharp/Parrot/AfricanParrot.cs
+++ CSharp/Parrot/AfricanParrot.cs
@@ -4,13 +4,11 @@ namespace Parrot;
 
 public class AfricanParrot : IParrot
 {
-    private readonly Parrot _parrot;
     private readonly int _numberOfCoconuts;
 
     public AfricanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, bool isNailed)
     {
         _numberOfCoconuts = numberOfCoconuts;
-        _parrot = new Parrot(type, numberOfCoconuts, voltage, isNailed);
     }
 
     public double GetSpeed()

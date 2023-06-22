3485bbe78dd34da3fe344ffa87140fc7c01ecad9
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-06-22 14:07:02 +0100

    Put new class into its own file

--- CSharp/Parrot/Parrot.cs
+++ CSharp/Parrot/Parrot.cs
@@ -77,24 +77,4 @@ namespace Parrot
             return value;
         }
     }
-
-    public class NorwegianBlueParrot : IParrot
-    {
-        private readonly Parrot _parrot;
-
-        public NorwegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, bool isNailed)
-        {
-            _parrot = new Parrot(type, numberOfCoconuts, voltage, isNailed);
-        }
-
-        public double GetSpeed()
-        {
-            return _parrot.GetSpeed();
-        }
-
-        public string GetCry()
-        {
-            return _parrot.GetCry();
-        }
-    }
 }

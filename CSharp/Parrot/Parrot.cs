5c291f9499a2913b77e6c91fb3b5b9c6b6733200
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-06-22 13:29:08 +0100

    Move AfricanParrot to its own file

--- CSharp/Parrot/Parrot.cs
+++ CSharp/Parrot/Parrot.cs
@@ -80,24 +80,4 @@ namespace Parrot
             return value;
         }
     }
-
-    public class AfricanParrot : IParrot
-    {
-        private readonly Parrot _parrot;
-
-        public AfricanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, bool isNailed)
-        {
-            _parrot = new Parrot(type, numberOfCoconuts, voltage, isNailed);
-        }
-
-        public double GetSpeed()
-        {
-            return _parrot.GetSpeed();
-        }
-
-        public string GetCry()
-        {
-            return _parrot.GetCry();
-        }
-    }
 }

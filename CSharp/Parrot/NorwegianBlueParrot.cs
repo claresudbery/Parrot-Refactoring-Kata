df942dcdf15b515ebd50e35ea18b8dc1f17946a0
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-06-22 14:12:53 +0100

    Remove unnecessary member var

--- CSharp/Parrot/NorwegianBlueParrot.cs
+++ CSharp/Parrot/NorwegianBlueParrot.cs
@@ -4,13 +4,11 @@ public class NorwegianBlueParrot : IParrot
 {
     private readonly double _voltage;
     private readonly bool _isNailed;
-    private readonly Parrot _parrot;
 
     public NorwegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, bool isNailed)
     {
         _voltage = voltage;
         _isNailed = isNailed;
-        _parrot = new Parrot(type, numberOfCoconuts, voltage, isNailed);
     }
 
     public double GetSpeed()

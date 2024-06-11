72eb29129893c74fa7c415f294deb97f5e3fd558
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-06-11 16:23:25 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:CSharp/Parrot/NorwegianBlueParrot.cs

--- CSharp/Parrot/AfricanParrot.cs
+++ CSharp/Parrot/AfricanParrot.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Parrot;
 
 public class AfricanParrot : Parrot
@@ -5,5 +7,20 @@ public class AfricanParrot : Parrot
     public AfricanParrot(int numberOfCoconuts, double voltage, bool isNailed): 
         base(ParrotTypeEnum.AFRICAN, numberOfCoconuts, voltage, isNailed)
     {
+    }
+
+    public override double GetSpeed()
+    {
+        switch (_type)
+        {
+            case ParrotTypeEnum.EUROPEAN:
+                return GetBaseSpeed();
+            case ParrotTypeEnum.AFRICAN:
+                return Math.Max(0, GetBaseSpeed() - GetLoadFactor() * _numberOfCoconuts);
+            case ParrotTypeEnum.NORWEGIAN_BLUE:
+                return _isNailed ? 0 : GetBaseSpeed(_voltage);
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
